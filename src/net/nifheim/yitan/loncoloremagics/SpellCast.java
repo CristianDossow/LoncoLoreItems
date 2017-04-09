ab6eb7b4127f86801eac3e73150ac85e1ca13d70
Beelzebu <jaime.pinkfloyd@hotmail.com>
2017-04-08 23:59:18 -0300

    e.e

--- src/net/nifheim/yitan/loncoloremagics/SpellCast.java
+++ src/net/nifheim/yitan/loncoloremagics/SpellCast.java
@@ -1,62 +1,53 @@
 package net.nifheim.yitan.loncoloremagics;
 
-import java.io.File;
-
-import org.bukkit.Effect;
 import org.bukkit.Location;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.DragonFireball;
-import org.bukkit.entity.Egg;
-import org.bukkit.entity.Fireball;
 import org.bukkit.entity.LlamaSpit;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
-import org.bukkit.entity.SmallFireball;
-import org.bukkit.entity.Snowball;
 import org.bukkit.metadata.FixedMetadataValue;
 
 import net.nifheim.yitan.itemlorestats.Main;
 
 public class SpellCast {
-	   public static void spellBuilder(Spell spell, Player player)
-	   {
-	     Projectile projectile = getProjectile(spell, player);
-	     projectile.setShooter(player);
-	     projectile.setVelocity(player.getLocation().getDirection().multiply(spell.speed));
-	     setProjectileProperties(projectile, spell);
-	   }
-	   
-	   public static Projectile getProjectile(Spell spell,Player player) {
-	     Location loc = player.getEyeLocation().toVector().add(player.getLocation().getDirection().multiply(2)).toLocation(player.getWorld(), player.getLocation().getYaw(), player.getLocation().getPitch());
-	     
-	     LlamaSpit projectile = (LlamaSpit)player.getWorld().spawn(loc, LlamaSpit.class);
-	     projectile.setGravity(false);
-	     projectile.setGlowing(true);
-	     //projectile.setMetadata("ILS_DragonFireball", new FixedMetadataValue(Main.getPlugin(), player.getName()));
-	     return projectile;
-	     
-	   }
-	   
-	   public static void setProjectileProperties(Projectile projectile, Spell spell) {
-	     double DHA = spell.directHeal;
-	     double AHA = spell.aoeHealAmount;
-	     double DDA = spell.directDamageAmount;
-	     double ADA = spell.aoeDamageAmount;
-	     
-	     projectile.setMetadata("SPELLNAME=", new FixedMetadataValue(Main.getPlugin(), spell.name));
-	     projectile.setMetadata("DHA=", new FixedMetadataValue(Main.getPlugin(), Double.valueOf(DHA)));
-	     projectile.setMetadata("AHA=", new FixedMetadataValue(Main.getPlugin(), Double.valueOf(AHA)));
-	     projectile.setMetadata("AHR=", new FixedMetadataValue(Main.getPlugin(), Double.valueOf(spell.aoeHealRange)));
-	     projectile.setMetadata("DDA=", new FixedMetadataValue(Main.getPlugin(), Double.valueOf(DDA)));
-	     projectile.setMetadata("ADA=", new FixedMetadataValue(Main.getPlugin(), Double.valueOf(ADA)));
-	     projectile.setMetadata("ADR=", new FixedMetadataValue(Main.getPlugin(), Double.valueOf(spell.aoeDamageRange)));
-	     
-	     if ((DHA > 0.0D) || (AHA > 0.0D)) {
-	       projectile.setMetadata("Heal=", new FixedMetadataValue(Main.getPlugin(), Boolean.valueOf(true)));
-	     }
-	     
-	     if ((DDA > 0.0D) || (ADA > 0.0D)) {
-	       projectile.setMetadata("Damage=", new FixedMetadataValue(Main.getPlugin(), Boolean.valueOf(true)));
-	     }
-	   }
+
+    public static void spellBuilder(Spell spell, Player player) {
+        Projectile projectile = getProjectile(spell, player);
+        projectile.setShooter(player);
+        projectile.setVelocity(player.getLocation().getDirection().multiply(spell.speed));
+        setProjectileProperties(projectile, spell);
+    }
+
+    public static Projectile getProjectile(Spell spell, Player player) {
+        Location loc = player.getEyeLocation().toVector().add(player.getLocation().getDirection().multiply(2)).toLocation(player.getWorld(), player.getLocation().getYaw(), player.getLocation().getPitch());
+
+        LlamaSpit projectile = (LlamaSpit) player.getWorld().spawn(loc, LlamaSpit.class);
+        projectile.setGravity(false);
+        //projectile.setGlowing(true);
+        //projectile.setMetadata("ILS_DragonFireball", new FixedMetadataValue(Main.getPlugin(), player.getName()));
+        return projectile;
+
+    }
+
+    public static void setProjectileProperties(Projectile projectile, Spell spell) {
+        double DHA = spell.directHeal;
+        double AHA = spell.aoeHealAmount;
+        double DDA = spell.directDamageAmount;
+        double ADA = spell.aoeDamageAmount;
+
+        projectile.setMetadata("SPELLNAME=", new FixedMetadataValue(Main.getPlugin(), spell.name));
+        projectile.setMetadata("DHA=", new FixedMetadataValue(Main.getPlugin(), DHA));
+        projectile.setMetadata("AHA=", new FixedMetadataValue(Main.getPlugin(), AHA));
+        projectile.setMetadata("AHR=", new FixedMetadataValue(Main.getPlugin(), spell.aoeHealRange));
+        projectile.setMetadata("DDA=", new FixedMetadataValue(Main.getPlugin(), DDA));
+        projectile.setMetadata("ADA=", new FixedMetadataValue(Main.getPlugin(), ADA));
+        projectile.setMetadata("ADR=", new FixedMetadataValue(Main.getPlugin(), spell.aoeDamageRange));
+
+        if ((DHA > 0.0D) || (AHA > 0.0D)) {
+            projectile.setMetadata("Heal=", new FixedMetadataValue(Main.getPlugin(), true));
+        }
+
+        if ((DDA > 0.0D) || (ADA > 0.0D)) {
+            projectile.setMetadata("Damage=", new FixedMetadataValue(Main.getPlugin(), true));
+        }
+    }
 }
--- src/net/nifheim/yitan/loncoloremagics/SpellListeners.java
+++ src/net/nifheim/yitan/loncoloremagics/SpellListeners.java
@@ -8,30 +8,29 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class SpellListeners implements Listener{
-	static String languageRegex= "[^A-Za-zñÑáéíóúÁÉÍÓÚ_]";
-	
+public class SpellListeners implements Listener {
+
+    static String languageRegex = "[^A-Za-zñÑáéíóúÁÉÍÓÚ_]";
+
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-    	if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)|| event.getAction().equals(Action.RIGHT_CLICK_AIR)){
+        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
             ItemStack offhand = event.getPlayer().getInventory().getItemInOffHand();
             if (offhand != null && offhand.getType().equals(Material.ENCHANTED_BOOK)) {
-            	if(offhand.hasItemMeta())
-            		if(offhand.getItemMeta().hasLore()){
-            			String spellname = offhand.getItemMeta().getLore().get(0);
-            			spellname = ChatColor.stripColor(spellname.toString());
-            			Spell spell = SpellsList.getSpell(spellname);
-            			if (spell!=null){
-            				event.getPlayer().sendMessage("lanzando "+spell.name);
-            				SpellCast.spellBuilder(spell, event.getPlayer());
-            			}
-            			else
-            				event.getPlayer().sendMessage("no contiene echizos");
-            			
-            		}
-            			
+                if (offhand.hasItemMeta()) {
+                    if (offhand.getItemMeta().hasLore()) {
+                        String spellname = offhand.getItemMeta().getLore().get(0);
+                        spellname = ChatColor.stripColor(spellname);
+                        Spell spell = SpellsList.getSpell(spellname);
+                        if (spell != null) {
+                            event.getPlayer().sendMessage("lanzando " + spell.name);
+                            SpellCast.spellBuilder(spell, event.getPlayer());
+                        } else {
+                            event.getPlayer().sendMessage("no contiene echizos");
+                        }
+                    }
+                }
             }
-    	}
-
+        }
     }
 }
