48c55f689dbe2903cde389ec5fa80eccef749501
BuildTools <cristiandossow@gmail.com>
2017-04-10 12:48:19 -0300

    new PlayerStats class

---
+++ src/net/nifheim/yitan/itemlorestats/PlayerStats.java
@@ -0,0 +1,48 @@
+package net.nifheim.yitan.itemlorestats;
+
+import java.util.UUID;
+
+public class PlayerStats {
+	UUID uuid;
+	String name;
+	//int lvl;  - se ocupará directo de la clase player
+	double baseDamage; //base 1, podria ser modificable mediante nivel o clase
+	double minDamage;
+	double maxDamage;
+	int armor;
+	double percentArmor;
+	double dodge; //maximo 80%, activable 1 vez cada 2 seg, no daña durabilidad armadura
+	double block; //maximo 50%, activable 1 vez cada 2 seg + lentiud del receptor, daña solo durabilidad del escudo
+	double critChance; //base según arma
+	double critDamage; //el base es 50% + bonos del set
+	double healthRegen; //no estudiado
+	double lifeSteal;  //sin ocupar
+	double reflect; //no estudiado y sin ocupar
+	double fire; //sin ocupar
+	double ice; //no estudiado y sin ocupar
+	double poison; //cuando es un mob inmune a veneno, se reemplaza por wither
+	double wither; //sin ocupar
+	double harming; //no estudiado y sin ocupar
+	double blind; //no estudiado y sin ocupar
+	double XPMultiplier; //??
+	double movementSpeed;
+	
+	// stats por programar --------------------
+	
+	double manaMax;
+	double manaCurrent;
+	double manaRegen;
+	double spellCooldownReduction; //maximo 50%, reduccion tiempo entre lanzamientos
+	double magicDamage; //la aletoriedad del daño dependerá del echizo
+	int magicArmor;  
+	double magicPercentArmor; 
+	double armorPen; // penetración de armadura
+	double magicArmorPen; // penetración de armadura mágica
+	double stab; // probabilidad de apuñalar por detras, stat para dagas
+	double stabDamage; //multiplicador del stab
+	
+	
+	
+	
+
+}
