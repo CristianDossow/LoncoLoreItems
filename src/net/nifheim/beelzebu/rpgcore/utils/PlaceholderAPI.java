9a85e67363771e357d09385adac41d135f11f557
Beelzebu <jaime.pinkfloyd@hotmail.com>
2017-04-18 01:40:20 -0300

    Pom cleanup and New placeholder

--- src/net/nifheim/beelzebu/rpgcore/utils/PlaceholderAPI.java
+++ src/net/nifheim/beelzebu/rpgcore/utils/PlaceholderAPI.java
@@ -1,26 +1,31 @@
 package net.nifheim.beelzebu.rpgcore.utils;
 
+import java.text.DecimalFormat;
 import net.nifheim.yitan.itemlorestats.Main;
 import me.clip.placeholderapi.external.EZPlaceholderHook;
 import org.bukkit.entity.Player;
 
-
 public class PlaceholderAPI extends EZPlaceholderHook {
+
     private final Main plugin;
+
     public PlaceholderAPI(Main plugin) {
         super(plugin, "rpgcore");
         this.plugin = plugin;
     }
 
+    DecimalFormat df = new DecimalFormat("#.#");
     @Override
     public String onPlaceholderRequest(Player p, String str) {
         if (p == null) {
             return "Player is needed!";
         }
         if (str.equals("mana")) {
-            return String.valueOf(plugin.getPlayerStats(p).manaCurrent);
+            return String.valueOf(df(plugin.getPlayerStats(p).manaCurrent));
         }
-        else {
+        if (str.equals("mana_max")) {
+            return String.valueOf(df(plugin.getPlayerStats(p).manaMax));
+        } else {
             return "Invalid Placeholder";
         }
     }
