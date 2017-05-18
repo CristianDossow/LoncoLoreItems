8ec51c9764144e7aa47b2649b4e8cdfd7605f84a
Beelzebu <jaime.pinkfloyd@hotmail.com>
2017-05-18 10:46:40 -0400

    Removed unused imports

--- src/net/nifheim/yitan/loncoloremagics/SpellParticles.java
+++ src/net/nifheim/yitan/loncoloremagics/SpellParticles.java
@@ -1,10 +1,8 @@
 package net.nifheim.yitan.loncoloremagics;
 
-import org.bukkit.Sound;
 import org.bukkit.entity.Projectile;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import de.slikey.effectlib.Effect;
 import de.slikey.effectlib.EffectManager;
 import de.slikey.effectlib.effect.ConeEffect;
 import de.slikey.effectlib.effect.DragonEffect;
@@ -12,7 +10,6 @@ import de.slikey.effectlib.effect.SphereEffect;
 import de.slikey.effectlib.util.ParticleEffect;
 import net.nifheim.yitan.itemlorestats.Main;
 import net.nifheim.yitan.itemlorestats.listeners.magicProjectileHit;
-
 
 public class SpellParticles extends BukkitRunnable {
 
@@ -30,76 +27,76 @@ public class SpellParticles extends BukkitRunnable {
         this.spell = spell;
         this.startime = System.currentTimeMillis();
         this.maxTime = spell.lifeTime;
-        em= new EffectManager(plugin);
-        if(spell.particleEffectSphere!=null){
-        	for(ParticleEffect peffect : spell.particleEffectSphere){
-            SphereEffect effect = new SphereEffect(em); 
-            effect.particle=peffect;
-            //effect.particleCount = 2;
-            effect.particles=spell.particlesAmount;
-            effect.radius=spell.particleEffectSphereradio;
-    		effect.setEntity(projectile);
-    		effect.delay=0;
-    		effect.speed=spell.particleSpeed;
-    		effect.period = spell.Interval;
-    		effect.start();
-        	}
+        em = new EffectManager(plugin);
+        if (spell.particleEffectSphere != null) {
+            for (ParticleEffect peffect : spell.particleEffectSphere) {
+                SphereEffect effect = new SphereEffect(em);
+                effect.particle = peffect;
+                //effect.particleCount = 2;
+                effect.particles = spell.particlesAmount;
+                effect.radius = spell.particleEffectSphereradio;
+                effect.setEntity(projectile);
+                effect.delay = 0;
+                effect.speed = spell.particleSpeed;
+                effect.period = spell.Interval;
+                effect.start();
+            }
         }
     }
-	
-	@Override
-	public void run() {
-		
-		if(startime+maxTime<System.currentTimeMillis()){
-			em.dispose();
-			this.cancel();
-			projectile.remove();
-		}
-		if(projectile.isDead()){
-			em2= new EffectManager(instance);
-            ParticleEffect peffect=null;
+
+    @Override
+    public void run() {
+
+        if (startime + maxTime < System.currentTimeMillis()) {
+            em.dispose();
+            this.cancel();
+            projectile.remove();
+        }
+        if (projectile.isDead()) {
+            em2 = new EffectManager(instance);
+            ParticleEffect peffect = null;
             peffect = spell.hitEffect;
-            if(spell.soundOnCast!=null){
-            	projectile.getLocation().getWorld().playSound(projectile.getLocation(), spell.soundOnHit, 1, 1);
+            if (spell.soundOnCast != null) {
+                projectile.getLocation().getWorld().playSound(projectile.getLocation(), spell.soundOnHit, 1, 1);
             }
-            if(spell.onHitType ==1){
-            	DragonEffect effect = new DragonEffect(em2);
-                effect.particle=peffect;
-                effect.particles=spell.particlesAmountOnHit;
+            if (spell.onHitType == 1) {
+                DragonEffect effect = new DragonEffect(em2);
+                effect.particle = peffect;
+                effect.particles = spell.particlesAmountOnHit;
                 //effect.radius=spell.particleEffectSphereradio;
-                effect.iterations=1;
+                effect.iterations = 1;
                 effect.setLocation(projectile.getLocation());
-        		effect.delay=0;
-        		effect.speed=spell.particleSpeedOnHit;
-        		effect.start();
+                effect.delay = 0;
+                effect.speed = spell.particleSpeedOnHit;
+                effect.start();
             }
-            if(spell.onHitType ==2){
-            	SphereEffect effect = new SphereEffect(em2);
-                effect.particle=peffect;
-                effect.particles=spell.particlesAmountOnHit;
-                effect.radius=spell.particleRadioOnhit;
-                effect.iterations=1;
+            if (spell.onHitType == 2) {
+                SphereEffect effect = new SphereEffect(em2);
+                effect.particle = peffect;
+                effect.particles = spell.particlesAmountOnHit;
+                effect.radius = spell.particleRadioOnhit;
+                effect.iterations = 1;
                 effect.setLocation(projectile.getLocation());
-        		effect.delay=0;
-        		effect.speed=spell.particleSpeedOnHit;
-        		effect.start();
+                effect.delay = 0;
+                effect.speed = spell.particleSpeedOnHit;
+                effect.start();
             }
-            if(spell.onHitType ==3){
-            	ConeEffect effect = new ConeEffect(em2);
-                effect.particle=peffect;
-                effect.particles=spell.particlesAmountOnHit;
+            if (spell.onHitType == 3) {
+                ConeEffect effect = new ConeEffect(em2);
+                effect.particle = peffect;
+                effect.particles = spell.particlesAmountOnHit;
                 effect.radiusGrow = (float) spell.particleRadioOnhit;
-                effect.iterations=1;
+                effect.iterations = 1;
                 effect.setLocation(projectile.getLocation());
-        		effect.delay=0;
-        		effect.speed=spell.particleSpeedOnHit;
-        		effect.start();
+                effect.delay = 0;
+                effect.speed = spell.particleSpeedOnHit;
+                effect.start();
             }
-			em.dispose();
-			this.cancel();
-			magicProjectileHit.onProjectileHitEvent(projectile);
-			projectile.remove();
-		}
-	}
+            em.dispose();
+            this.cancel();
+            magicProjectileHit.onProjectileHitEvent(projectile);
+            projectile.remove();
+        }
+    }
 
 }
