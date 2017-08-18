ec6ba704fd82a178a001ce4a0a56053f8f018088
Beelzebu <jaime.pinkfloyd@hotmail.com>
2017-08-18 20:56:05 -0300

    Updated TestCommand

--- src/net/nifheim/beelzebu/rpgcore/commands/TestCommand.java
+++ src/net/nifheim/beelzebu/rpgcore/commands/TestCommand.java
@@ -1,9 +1,5 @@
 package net.nifheim.beelzebu.rpgcore.commands;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import net.nifheim.beelzebu.rpgcore.utils.InventorySerializer;
 import net.nifheim.yitan.itemlorestats.Main;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -15,9 +11,9 @@ import org.bukkit.entity.Player;
  * @author Beelzebu
  */
 public class TestCommand implements CommandExecutor {
-    
+
     private final Main plugin;
-    
+
     public TestCommand(Main main) {
         plugin = main;
     }
@@ -27,15 +23,12 @@ public class TestCommand implements CommandExecutor {
         if (sender instanceof Player) {
             Player p = (Player) sender;
             if (args[0].equalsIgnoreCase("items")) {
-                plugin.getConfig().set("Test.ItemSerialized", InventorySerializer.serialize(p.getInventory().getItemInMainHand()));
+                plugin.getConfig().set("Test.ItemSerialized", p.getInventory().getItemInMainHand());
+                plugin.saveConfig();
+                plugin.reloadConfig();
                 p.sendMessage("serialized");
             } else if (args[0].equalsIgnoreCase("itemd")) {
-                try {
-                    InventorySerializer.deserialize(plugin.getConfig().get("Tests.ItemSerialized"));
-                    p.sendMessage("deserialized");
-                } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
-                    Logger.getLogger(TestCommand.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                p.getInventory().setItemInMainHand(plugin.getConfig().getItemStack("Test.ItemSerialized"));
             } else {
                 p.sendMessage("");
             }
