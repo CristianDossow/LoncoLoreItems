b87ff5e371497b6ddab59cc9c097f20f2fb0ddb9
Beelzebu <jaime.pinkfloyd@hotmail.com>
2017-08-18 20:55:05 -0300

    Updated to 1.12

--- src/net/nifheim/beelzebu/rpgcore/utils/ActionBarAPI.java
+++ src/net/nifheim/beelzebu/rpgcore/utils/ActionBarAPI.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import net.nifheim.yitan.itemlorestats.Main;
 
@@ -25,34 +26,35 @@ public class ActionBarAPI {
     }
 
     public static void sendActionBar(Player player, String message) {
+        if (!player.isOnline()) {
+            return; // Player may have logged out
+        }
 
         try {
-            Class<?> c1 = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
-            Object p = c1.cast(player);
+            Class<?> craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + nmsver + ".entity.CraftPlayer");
+            Object craftPlayer = craftPlayerClass.cast(player);
             Object ppoc;
             Class<?> c4 = Class.forName("net.minecraft.server." + nmsver + ".PacketPlayOutChat");
             Class<?> c5 = Class.forName("net.minecraft.server." + nmsver + ".Packet");
-            if (useOldMethods) {
-                Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatSerializer");
-                Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-                Method m3 = c2.getDeclaredMethod("a", String.class);
-                Object cbc = c3.cast(m3.invoke(c2, "{\"text\": \"" + message + "\"}"));
-                ppoc = c4.getConstructor(new Class<?>[]{c3, byte.class}).newInstance(cbc, (byte) 2);
-            } else {
-                Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
-                Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
-                Object o = c2.getConstructor(new Class<?>[]{String.class}).newInstance(message);
-                ppoc = c4.getConstructor(new Class<?>[]{c3, byte.class}).newInstance(o, (byte) 2);
+            Class<?> c2 = Class.forName("net.minecraft.server." + nmsver + ".ChatComponentText");
+            Class<?> c3 = Class.forName("net.minecraft.server." + nmsver + ".IChatBaseComponent");
+            Class<?> chatMessageTypeClass = Class.forName("net.minecraft.server." + nmsver + ".ChatMessageType");
+            Object[] chatMessageTypes = chatMessageTypeClass.getEnumConstants();
+            Object chatMessageType = null;
+            for (Object obj : chatMessageTypes) {
+                if (obj.toString().equals("GAME_INFO")) {
+                    chatMessageType = obj;
+                }
             }
-            Method m1 = c1.getDeclaredMethod("getHandle");
-            Object h = m1.invoke(p);
+            Object o = c2.getConstructor(new Class<?>[]{String.class}).newInstance(message);
+            ppoc = c4.getConstructor(new Class<?>[]{c3, chatMessageTypeClass}).newInstance(o, chatMessageType);
+            Method m1 = craftPlayerClass.getDeclaredMethod("getHandle");
+            Object h = m1.invoke(craftPlayer);
             Field f1 = h.getClass().getDeclaredField("playerConnection");
             Object pc = f1.get(h);
             Method m5 = pc.getClass().getDeclaredMethod("sendPacket", c5);
             m5.invoke(pc, ppoc);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            works = false;
+        } catch (ClassNotFoundException | IllegalAccessException | IllegalArgumentException | InstantiationException | NoSuchFieldException | NoSuchMethodException | SecurityException | InvocationTargetException ex) {
         }
     }
 
